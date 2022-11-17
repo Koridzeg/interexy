52e88dcd1c6619fc0bebe2f75790f4f3dacb8281
KoridzeG <giorgiqori@yahoo.com>
2022-11-18 02:36:22 +0400

    feat: routing & suspense api & navbar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,23 @@
-import React from 'react';
+import React, { lazy, Suspense } from 'react';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import Navbar from './pages/global/Navbar';
+
+const Home = lazy(() => import('./pages/Home/Home'))
+const Login = lazy(() => import('./pages/Login/Login'))
+const Register = lazy(() => import('./pages/Register/Register'))
 
 function App() {
   return (
-    <div className="App">
-
-    </div>
+    <Suspense fallback={<div>...loading</div>}>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/register' element={<Register />} />
+          <Route path='/login' element={<Login />} />
+        </Routes>
+      </Router>
+    </Suspense>
   );
 }
 
---
+++ frontend/src/pages/global/Navbar.tsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import { Box } from "@mui/material"
+import { useNavigate } from 'react-router'
+import { IconButton } from '@mui/material';
+import LoginIcon from '@mui/icons-material/Login';
+const Navbar = () => {
+    const navigate = useNavigate()
+
+    return (
+        <Box
+            bgcolor="rgba(255,255,255,0.95)"
+            display="flex"
+            width="100%"
+            height="60px"
+            position="fixed"
+            zIndex="1"
+            top="0"
+            left="0"
+        >
+            <Box
+                width="70%"
+                margin="auto"
+                display="flex"
+                justifyContent="space-between"
+                alignItems="center"
+            >
+                <Box onClick={() => navigate('/')}
+                    sx={{ "&:hover": { cursor: "pointer" } }}
+                >
+                    Home
+                </Box>
+            </Box>
+        </Box>
+    )
+}
+
+export default Navbar
