ef12f2065b33082825efe9a8adb0ebd5390e024b
KoridzeG <giorgiqori@yahoo.com>
2022-11-17 14:30:18 +0400

    feat: connect database

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,18 @@
-require('dotenv').config();
-const express = require('express')
+require("dotenv").config();
+const express = require("express");
 const app = express();
+
+const connectDb = require("./db/connect");
 
 const port = process.env.PORT || 3000;
 
+const start = async () => {
+  try {
+    await connectDb(process.env.MONGO_URI);
+    app.listen(port, () => {
+      console.log(`Server is listening on port ${port}`);
+    });
+  } catch (error) {}
+};
+
+start();
